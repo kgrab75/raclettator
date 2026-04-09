8db61b6050b7aabc815a6bb9cffb68130bcd875e
kgrab75 <kgrabiner75@gmail.com>
2026-04-09 11:25:37 +0200

    ci: add playwright summary to github job overview

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['list'], ['github'], ['json', { outputFile: 'playwright-report/results.json' }], ['html', { open: 'never' }]]
+    : 'html',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
